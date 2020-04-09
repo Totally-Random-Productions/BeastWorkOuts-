0c6c7e293844182818c24c21ff405bfb49b84040
Shereece Victor <alexreecevictor@gmail.com>
2020-04-09 02:22:48 -0400

    Started the Login, Signup and Homepages using templates from online

--- Project/src/app.py
+++ Project/src/app.py
@@ -1,16 +1,29 @@
-from flask import Flask, request, jsonify, render_template, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/test")
 def hello():
     return "Hello World!"
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/upload")
-def uploadPage():
-    return render_template("upload.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/workouts")
+def workouts():
+    return render_template("workouts.html")
 
