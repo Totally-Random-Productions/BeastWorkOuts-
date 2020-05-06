53b2dc64fab16344cbc578b56a042becfaf221e3
Randy Deo <randyeo@gmail.com>
2020-05-05 23:22:42 -0400

    Added Card layout to Workouts page

--- Project/src/initDB.py
+++ Project/src/initDB.py
@@ -32,11 +32,20 @@ with open('exerciseList.csv') as workouts:
     itercsv = iter(readCSV)
     next(itercsv)
     for row in itercsv:
+
+        #Removes the name of the image and leaves url only
+        start = "("
+        end = ")"
+        example = row[5]
+        url = example[example.find(start) + len(start):example.rfind(end)]
+        #print(url)
+
         # print(row) For testing, remove when cleaning
         workoutDetails = Exercise(exerciseName=row[0], equipment=row[1],
                                   exerciseType=row[2], majorMuscle=row[3],
-                                  minorMuscle=row[4], example=row[5],
+                                  minorMuscle=row[4], example=url,
                                   notes=row[6], mods=row[7])
+
         db.session.add(workoutDetails)
     db.session.commit()
     print("Database Initialised!")
